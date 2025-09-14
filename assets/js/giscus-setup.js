3a3f4b74316f95a4c78cfda75560ee4e2b4ab771
hoytlong <hoytlong@uchicago.edu>
2025-09-14 16:10:53 +0000

    Deploying to gh-pages from @ culturalintelligencelab/culturalintelligencelab.github.io@879267289fe85fd3d79efe2e245263c584d6700f 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "culturalintelligencelab/culturalintelligencelab",
+    "data-repo": "culturalintelligencelab/culturalintelligencelab.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
