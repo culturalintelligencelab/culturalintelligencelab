f90cb8db6555d4a86ef04507f6f3b6b719e4b3eb
hoytlong <hoytlong@uchicago.edu>
2025-09-15 16:44:08 +0000

    Deploying to gh-pages from @ culturalintelligencelab/culturalintelligencelab.github.io@4e285d954192e1d298c5e79263f2655d62e1822d 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -403,10 +403,7 @@ ninja.data = [{
           description: "",
           section: "News",handler: () => {
               window.location.href = "/news/announcement_2/";
-            },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
-          title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
-          description: "",
-          section: "News",},{id: "news-cultural-intelligence-lab-website-goes-live",
+            },},{id: "news-cultural-intelligence-lab-website-goes-live",
           title: 'Cultural Intelligence Lab website goes live!',
           description: "",
           section: "News",},{id: "projects-project-1",
